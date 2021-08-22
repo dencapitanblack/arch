1e4e596a1cb784086a518b4c32619110176a2366
dencapitanblack <88950425+dencapitanblack@users.noreply.github.com>
2021-08-22 22:49:30 +0300

    Update installSkript.sh

--- installSkript.sh
+++ installSkript.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 #create a GUID Partition Table
-fdisk /dev/sda << EEOF
+fdisk /dev/sda << STDIO
 g
 w
-EEOF
+STDIO
+
+fdisk /dev/sda << STDIO
+n
+1
+2048
+411647
+w
+STDIO
+
+fdisk /dev/sda << STDIO
+n
+2
+411648
+812247
+t
+2
+4
+w
+STDIO
 
