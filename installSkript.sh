857123923e056a96c99c6bef6f8c133fb7214109
dencapitanblack <88950425+dencapitanblack@users.noreply.github.com>
2021-08-23 01:26:26 +0300

    Update installSkript.sh
    upd_04

--- installSkript.sh
+++ installSkript.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+
 #create a GUID Partition Table
 fdisk /dev/sda << STDIO
 g
 w
 STDIO
 
+#creating sections
 fdisk /dev/sda << STDIO
 n
 1
@@ -48,4 +50,21 @@ n
 w
 STDIO
 
-#End
+#formatting sections
+mkfs -t ext2 -L Boot /dev/sda1
+mkfs -t ext4 -L Root /dev/sda3
+mkfs -t ext4 -L Home /dev/sda4
+mkswap /dev/sda5
+
+#mounting partitions
+mount /dev/sda3/ /mnt
+mkdir /mnt/{boot,home}
+mount /dev/sda1 /mnt/boot
+mount /dev/sda4 /mnt/home
+swapon /dev/sda5
+
+#Installing basic packages
+pacstrap /mnt base linux linux-firmware base-devel
+
+#Creatin an FSTAB
+genfstab -U /mnt >> /mnt/etc/fstab
