7706430e8550c7242b8f938d961113844dcdd819
dencapitanblack <88950425+dencapitanblack@users.noreply.github.com>
2021-08-22 23:05:07 +0300

    Update installSkript.sh

--- installSkript.sh
+++ installSkript.sh
@@ -17,10 +17,33 @@ fdisk /dev/sda << STDIO
 n
 2
 411648
-812247
+821247
 t
 2
 4
 w
 STDIO
 
+fdisk /dev/sda << STDIO
+n
+3
+821248 
+9209855
+w
+STDIO
+
+fdisk /dev/sda << STDIO
+n
+4
+9209856
+14661631
+w
+STDIO
+
+fdisk /dev/sda << STDIO
+n
+4
+14661632
+16777182
+w
+STDIO
