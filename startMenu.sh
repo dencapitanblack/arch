9368e550dcec051a952601f197a9951b5de06841
dencapitanblack <88950425+dencapitanblack@users.noreply.github.com>
2021-08-23 23:35:01 +0300

    Update startMenu.sh

--- startMenu.sh
+++ startMenu.sh
@@ -3,34 +3,35 @@
 pacman -Sy libnewt --noconfirm
 
 function startMenu() {
-    
+
     TOGSEL=$(whiptail --title "Menu" --fb --menu "Select option" 15 60 4 \
-    "1" "Option" \
+    "1" "Create GPT and and creating sections" \
     "2" "Option" \
-    "3" "Option" 3>&1 1>&2 2>&3)  
-    
+    "3" "Option" 3>&1 1>&2 2>&3)
+
     case $TOGSEL in
-    
+
     1)
-      echo "Option 1"
-      whiptail --title "Option 1" --msgbox "you choose options 1" 8 45
-      
+      echo "Create GPT and and creating sections"
+      chmod ugo+rwx gptWithSection.sh
+      ./gptWithSection.sh
+
       ;;
-    
+
     2)
       echo "Option 2"
       whiptail --title "Option 2" --msgbox "you chose options 2" 8 45
-      
+
       ;;
-    
+
     3)
       echo "Option 3"
-      whiptail --title "Option 3" --msgbox "you chose options 3" 8 45 
-      
+      whiptail --title "Option 3" --msgbox "you chose options 3" 8 45
+
       ;;
-    
+
     esac
-    
+
 }
 
 startMenu
