5c0857f1f120fe5e6641dd1fb2cff50171d4bac3
dencapitanblack <88950425+dencapitanblack@users.noreply.github.com>
2021-08-23 17:46:47 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,10 @@
 pacman -S dialog --noconfirm
 tempfile="$(mktemp)"
 while true; do
-    dialog --menu 'Please take a selection' 18 70 15 $(n1 
+    dialog --menu 'Please take a selection' 18 70 15 $(n1 selection.list) 2>"$tempfile" && break
+done
+n="$(cat "$tempfile")"
+value="$(sed -n "${n}p" selection.list)"
+rm "$tempfile"
+echo "The user selected option number $n: '$value'"
 
