d1f1d2a0322bcb9631f1f2fe734b552e74dc13b6
dencapitanblack <88950425+dencapitanblack@users.noreply.github.com>
2021-08-23 14:04:56 +0300

    Update installSkript.sh
    upd_10

--- installSkript.sh
+++ installSkript.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-#create a GUID Partition Table
+#create a GUID Partition Table and section
 fdisk /dev/sda << STDIO
 g
-w
-STDIO
-
-#creating sections
-fdisk /dev/sda << STDIO
 n
 1
 2048
 411647
-w
-STDIO
-
-fdisk /dev/sda << STDIO
 n
 2
 411648
@@ -23,27 +14,14 @@ n
 t
 2
 4
-w
-STDIO
-
-fdisk /dev/sda << STDIO
 n
 4
 9209856
 14661631
-w
-STDIO
-
-fdisk /dev/sda << STDIO
 n
 5
 14661632
 16777182
-w
-STDIO
-
-
-fdisk /dev/sda << STDIO
 n
 3
 
@@ -52,20 +30,20 @@ w
 STDIO
 
 #formatting sections
-mkfs -t ext2 -L Boot /dev/sda1
-mkfs -t ext4 -L Root /dev/sda3
-mkfs -t ext4 -L Home /dev/sda4
-mkswap /dev/sda5
+# mkfs -t ext2 -L Boot /dev/sda1
+# mkfs -t ext4 -L Root /dev/sda3
+# mkfs -t ext4 -L Home /dev/sda4
+# mkswap /dev/sda5
 
-#mounting partitions
-mount /dev/sda3/ /mnt
-mkdir /mnt/{boot,home}
-mount /dev/sda1 /mnt/boot
-mount /dev/sda4 /mnt/home
-swapon /dev/sda5
+# #mounting partitions
+# mount /dev/sda3/ /mnt
+# mkdir /mnt/{boot,home}
+# mount /dev/sda1 /mnt/boot
+# mount /dev/sda4 /mnt/home
+# swapon /dev/sda5
 
-#Installing basic packages
-pacstrap /mnt base linux linux-firmware
+# #Installing basic packages
+# pacstrap /mnt base linux linux-firmware
 
-#Creatin an FSTAB
-genfstab -U /mnt >> /mnt/etc/fstab
+# #Creatin an FSTAB
+# genfstab -U /mnt >> /mnt/etc/fstab
