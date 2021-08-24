82af536e79f5cb0a0269ebcaa394ff60c8acd495
dencapitanblack <88950425+dencapitanblack@users.noreply.github.com>
2021-08-24 11:37:53 +0300

    Update startMenu.sh

--- startMenu.sh
+++ startMenu.sh
@@ -5,9 +5,10 @@ pacman -Sy libnewt --noconfirm
 function startMenu() {
 
     TOGSEL=$(whiptail --title "Menu" --fb --menu "Select option" 15 60 4 \
-    "1" "Create GPT and and creating sections" \
-    "2" "Option" \
-    "3" "Option" 3>&1 1>&2 2>&3)
+    "1" "Create GPT and creating sections" \
+    "2" "Mounting partition and install base linux packs" \
+    "3" "Enter to arch-chroot" \
+    "4" "Option" 3>&1 1>&2 2>&3)
 
     case $TOGSEL in
 
@@ -16,21 +17,31 @@ function startMenu() {
       chmod ugo+rwx gptWithSection.sh
       ./gptWithSection.sh 2>> std-err.log 1>> std-out.log
       startMenu
-      ;;
-
-    2)
-      echo "Option 2"
-      chmod ugo+rwx formMount.sh
-      ./formMount.sh 2>> std-err.log 1>> std-out.log
-      startMenu
-      ;;
-
-    3)
-      echo "Option 3"
-      arch-chroot /mnt
       
       ;;
 
+    2)
+      echo "Mounting partition and install base linux packs"
+      chmod ugo+rwx formMount.sh
+      ./formMount.sh 2>> std-err.log 1>> std-out.log
+      startMenu
+      
+      ;;
+
+    3)
+      echo "Enter to arch-chroot"
+      arch-chroot /mnt
+      startMenu
+      
+      ;;
+      
+    4)
+      echo "Welcome to Bash $BASH_VERSION" > test_textbox
+      whiptail --textbox test_textbox 12 80
+      
+      ;;
+      
+      
     esac
 
 }
